91949c4a7f0783edae3c1adf99999c35b7ba02cf
mugilanmettuthangal <30741644+mugilanmettuthangal@users.noreply.github.com>
2017-08-05 11:08:49 +0530

    py

---
+++ ifelse.py
@@ -0,0 +1,10 @@
+x=int(integer("please enter an integer:"))
+if x<0;
+  x=0
+  print("negative to zero")
+  else x==0;
+  print('zero')
+  else x==1;
+  print('one')
+     elseif:
+     print('more')
